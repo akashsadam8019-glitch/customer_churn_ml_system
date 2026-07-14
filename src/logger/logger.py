d9ea0e807c4fca7cfcb768c0d3520d53deada3c8
akash <akashsadam8019@gmail.com>
2026-07-15 00:06:12 +0530

    Implement centralized logging module

---
+++ src/logger/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+
+from src.constants.constants import LOG_DIR
+
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+logger = logging.getLogger("CustomerChurnLogger")
