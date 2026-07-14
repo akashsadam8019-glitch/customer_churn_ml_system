a9391e60166dc3fea5ab489594631c4788b8bcf4
akash <akashsadam8019@gmail.com>
2026-07-15 01:47:29 +0530

    Save trained model using joblib

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import joblib
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
@@ -29,9 +31,32 @@ class ModelTrainer:
 
             accuracy = accuracy_score(y_test, predictions)
 
-            logger.info(f"Accuracy : {accuracy:.4f}")
+            logger.info(f"Accuracy: {accuracy:.4f}")
 
             return model, accuracy
+
+        except Exception as e:
+            logger.error(e)
+            raise CustomException(e, sys)
+
+    def save_model(self, model):
+
+        try:
+
+            logger.info("Saving trained model...")
+
+            os.makedirs("models", exist_ok=True)
+
+            model_path = os.path.join(
+                "models",
+                "customer_churn_model.pkl"
+            )
+
+            joblib.dump(model, model_path)
+
+            logger.info(f"Model saved at: {model_path}")
+
+            return model_path
 
         except Exception as e:
             logger.error(e)
